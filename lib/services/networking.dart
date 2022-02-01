f044ee22714318f540fed792fe1c294835355ba0
Ritika Tyagi <84351782+Ritikatyagi18@users.noreply.github.com>
2022-02-01 18:21:45 +0530

    Update networking.dart

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,8 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 class NetworkHelper{
-  String url='';
-  NetworkHelper(url);
+  final String url;
+  NetworkHelper(this.url);
 
   Future getData() async {
     http.Response response = await http.get(Uri.parse(url));
