e15e2bc5d7afc9d69df773baef3ad294228610b8
Ritika Tyagi <84351782+Ritikatyagi18@users.noreply.github.com>
2022-02-01 18:24:26 +0530

    Update air_quality.dart

--- lib/services/air_quality.dart
+++ lib/services/air_quality.dart
@@ -5,7 +5,7 @@ const apiKey = '8083dc42691bac1c34a34817f1726895';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/air_pollution';
 
 class AirPollutionModel  {
-    Future getLocationAQI() async {
+    Future <dynamic> getLocationAQI() async {
     Location location = Location();
     await location.getCurrentLocation();
     String url = '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey';
